7ff09ec5e98caabce8ec1a8dbe31fa5b0a31b7e0
JumiDeluxe <jmatuszewska00e@gmail.com>
2023-01-30 12:03:34 +0100

    updated API stage

--- tv_scripts/update_timestamp.sh
+++ tv_scripts/update_timestamp.sh
@@ -5,6 +5,6 @@ API_ID=""
 # provide device's VPN IP from wireguard cfg in an "IP" env var
 
 curl -v -X POST \
-        'https://'"$API_ID"'.execute-api.us-east-1.amazonaws.com/teststage/' \
+        'https://'"$API_ID"'.execute-api.us-east-1.amazonaws.com/alpha/' \
         -H 'content-type: application/json' \
         -d '{ "IP": "'"$IP"'"}'
